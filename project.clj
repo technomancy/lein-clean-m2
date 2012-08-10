25d589775ee757f8c3760fc192a2c18cbe52462a
Phil Hagelberg <technomancy@gmail.com>
2012-08-09 17:26:39 -0700

    Release 0.1.2.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-clean-m2 "0.1.1"
+(defproject lein-clean-m2 "0.1.2"
   :description "Remove unused artifacts from local repository."
   :url "https://github.com/technomancy/lein-clean-m2"
   :license {:name "Eclipse Public License"
